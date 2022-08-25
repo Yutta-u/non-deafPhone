61d5ad91561f08e11246c04f970fdcf0c78a3356
Yutta <111496203+Yutta-u@users.noreply.github.com>
2022-08-25 13:51:59 +0500

    Initial commit

---
+++ src/nondeafPhone.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class nondeafPhone {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        System.out.println(a);
+    }
+}
